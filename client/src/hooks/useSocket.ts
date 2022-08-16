1c84633f07062b330cd6e5e3936e3b33c02c0c0e
jintak0401 <jintak0401@naver.com>
2022-08-16 23:32:53 +0900

    feat: websocket 관련 훅 타입 추가

--- client/src/hooks/useSocket.ts
+++ client/src/hooks/useSocket.ts
@@ -1,12 +1,11 @@
-import io from 'socket.io-client';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
+import io, { Socket } from 'socket.io-client';
+// import dotenv from 'dotenv';
+// dotenv.config();
+//
 const SERVER_URL: string = process.env.REACT_APP_SERVER_URL as string;
 const socket = io(SERVER_URL);
 
-const useSocket = () => {
+const useSocket = (): Socket => {
   return socket;
 };
 
