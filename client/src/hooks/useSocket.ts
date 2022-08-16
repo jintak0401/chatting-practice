4f5f23fe502c9abf2326fc7c8ac8e7b61b55098d
jintak0401 <32933980+jintak0401@users.noreply.github.com>
2022-08-17 00:48:05 +0900

    [Feat / BE] 웹소켓을 이용한 통신 구현 (#4)
    * chore: 서버 웹소켓 라이브러리 추가
    
    * feat: 웹소켓에 namespace 추가
    
    * feat: CORS 제거
    
    * feat: namespace 및 key 추가
    
    * fix: 웹소켓 연결 2번 되는 에러 해결
    
    * feat: 웹소켓 로직 추가

--- client/src/hooks/useSocket.ts
+++ client/src/hooks/useSocket.ts
@@ -1,12 +1,8 @@
 import io, { Socket } from 'socket.io-client';
-// import dotenv from 'dotenv';
-// dotenv.config();
-//
 const SERVER_URL: string = process.env.REACT_APP_SERVER_URL as string;
-const socket = io(SERVER_URL);
 
-const useSocket = (): Socket => {
-  return socket;
+const useSocket = (namespace: string): Socket => {
+  return io(`${SERVER_URL}/${namespace}`);
 };
 
 export default useSocket;
