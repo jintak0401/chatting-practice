f0e391f900d0b5fb1c882fe3fb742da0a6a30793
jintak0401 <jintak0401@naver.com>
2022-08-19 08:53:05 +0900

    feat: ExceptionFilter 글로벌로 적용

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.setGlobalPrefix('api');
 
   await app.listen(8000);
 }
